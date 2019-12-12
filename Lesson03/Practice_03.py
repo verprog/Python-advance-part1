89b9bd741a2a08b9d19438ebbccc595500bff416
verprog <47172331+verprog@users.noreply.github.com>
2019-12-12 09:18:39 +0200

    Update Practice_03.py

--- Lesson03/Practice_03.py
+++ Lesson03/Practice_03.py
@@ -1,35 +1,28 @@
 import time
+
+
 def decorator(num_repeat):
     def actual_deccorator(func):
 
         def wrapper(range_start, range_end):
-            start_time = time.process_time()
+            start_time = time.time()
             result = []
             for i in range(num_repeat):
                 result.append(func(range_start, range_end))
+            print(f">> Function {str(func.__name__).upper()} - execution time: [{round((time.time() - start_time) * 1000, 3)} milliseconds]")
 
-                # def __new__(cls, *args, **kwargs):
-                #     obj = super().__new__(cls, *args, **kwargs)
-                #     return obj
-
-            print('Elapsed time during the whole function in seconds: ', time.process_time() - start_time)
             return result
 
         return wrapper
-        # total_time = time_start-time.time()
+
     return actual_deccorator
 
 
-
-@decorator(5)
+@decorator(4)
 def random_generator(range_start, range_end):
     import random
     return random.randint(range_start, range_end)
 
 
-result= random_generator(10,50)
+result = random_generator(10, 50)
 print(result)
-
-
-
-# Домашка - что такое стек, очередь, комлексное число реальная и мнимая часть
