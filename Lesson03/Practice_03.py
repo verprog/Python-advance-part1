260ed12d23b6cb3633d6f5bc95d00b4db60e9704
verprog <47172331+verprog@users.noreply.github.com>
2019-12-12 09:20:31 +0200

    Update Practice_03.py

--- Lesson03/Practice_03.py
+++ Lesson03/Practice_03.py
@@ -4,11 +4,11 @@ import time
 def decorator(num_repeat):
     def actual_deccorator(func):
 
-        def wrapper(range_start, range_end):
+        def wrapper(*args):
             start_time = time.time()
             result = []
             for i in range(num_repeat):
-                result.append(func(range_start, range_end))
+                result.append(func(*args))
             print(f">> Function {str(func.__name__).upper()} - execution time: [{round((time.time() - start_time) * 1000, 3)} milliseconds]")
 
             return result
